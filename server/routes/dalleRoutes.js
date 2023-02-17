eebcc809cf15e2ff888e48366a6a2048d4d0fe5a
Desubicator <francopirinoli@gmail.com>
2023-02-17 01:43:53 -0300

    fixex api response

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -28,8 +28,6 @@ router.route('/').post(async(req, res) => {
             response_format: "b64_json",
         });
 
-        res.json(aiResponse.data);
-
         const image = aiResponse.data.data[0].b64_json;
         res.status(200).json({ photo: image });
 
